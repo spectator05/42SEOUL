15bbcef6e714e843a1936c658daba95deba7695d
wjang <c6r9s8.42seoul.kr>
2020-02-09 03:55:47 +0900

    C08 solve ex01 and add ex04~05

---
+++ C/C08/ex05/ft_show_tab.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjang <wjang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/09 03:46:54 by wjang             #+#    #+#             */
+/*   Updated: 2020/02/09 03:54:58 by wjang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		write(1, &str[i++], 1);
+}
+
+void		ft_putnbr(int nb)
+{
+	char		c;
+	long long	i;
+
+	i = (long long)nb;
+	if (i < 0)
+	{
+		write(1, &"-", 1);
+		i *= -1;
+	}
+	if (0 <= i && i < 10)
+	{
+		c = i + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(i / 10);
+		ft_putnbr(i % 10);
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].size != 0)
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+}
---
+++ C/C08/ex05/ft_strs_to_tab.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjang <wjang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/09 03:17:31 by wjang             #+#    #+#             */
+/*   Updated: 2020/02/09 03:45:43 by wjang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+struct s_stock_str	*ft_strs_to_tab(int argc, char **argv);
+
+char				*ft_strdup(char *src)
+{
+	char	*res;
+	int		size;
+
+	size = 0;
+	while (src[size])
+		++size;
+	res = (char *)malloc((sizeof(char) * size + 1));
+	size = 0;
+	while (src[size])
+	{
+		res[size] = src[size];
+		size++;
+	}
+	res[size] = src[size];
+	return (res);
+}
+
+int					ft_strlen(char *str)
+{
+	int idx;
+
+	idx = 0;
+	while (*str != '\0')
+	{
+		idx++;
+		str++;
+	}
+	return (idx);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int argc, char **argv)
+{
+	t_stock_str	*struct_argv;
+	int			i;
+
+	if ((struct_argv = (t_stock_str *)
+	malloc(sizeof(t_stock_str) * (argc + 1))) == NULL)
+		return (0);
+	i = 0;
+	while (argv[i] && i < argc)
+	{
+		struct_argv[i].str = argv[i];
+		struct_argv[i].copy = ft_strdup(argv[i]);
+		struct_argv[i].size = ft_strlen(argv[i]);
+		i++;
+	}
+	struct_argv[i].str = 0;
+	struct_argv[i].copy = 0;
+	struct_argv[i].size = 0;
+	return (struct_argv);
+}
